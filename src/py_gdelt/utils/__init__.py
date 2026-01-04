6047e4406b15ae20757739830c069340b3f67c91
Robert Wasilewski <wasilewski.robert@gmail.com>
2026-01-04 03:21:32 +0100

    add utils

---
+++ src/py_gdelt/utils/__init__.py
@@ -0,0 +1,10 @@
+"""Utility functions for the py-gdelt library."""
+
+from py_gdelt.utils.dedup import DedupeStrategy, deduplicate
+from py_gdelt.utils.streaming import ResultStream
+
+__all__ = [
+    "DedupeStrategy",
+    "deduplicate",
+    "ResultStream",
+]
