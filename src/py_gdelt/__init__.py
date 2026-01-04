8330644fe24ee51dd4b5cb1cfb118baefe33aa88
Robert Wasilewski <wasilewski.robert@gmail.com>
2026-01-04 12:28:25 +0100

    add client

--- src/py_gdelt/__init__.py
+++ src/py_gdelt/__init__.py
@@ -4,6 +4,8 @@ py-gdelt: Python client library for GDELT (Global Database of Events, Language, 
 This library provides unified access to all GDELT data sources with a modern, type-safe API.
 """
 
+from py_gdelt.client import GDELTClient
+from py_gdelt.config import GDELTSettings
 from py_gdelt.exceptions import (
     APIError,
     APIUnavailableError,
@@ -23,6 +25,9 @@ __version__ = "0.1.0"
 __all__ = [
     # Version
     "__version__",
+    # Main client
+    "GDELTClient",
+    "GDELTSettings",
     # Exceptions
     "GDELTError",
     "APIError",
